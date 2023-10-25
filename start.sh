e10b637e78276036309a7ca2dab1e3ee52952620
SivaSoft <58784617+sivasoft-cloud@users.noreply.github.com>
2023-10-25 08:00:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 echo "Cloning Repo, Please Wait..."
 git clone https://github.com/sivasoft-cloud/Radio.git /Radio
 echo "Installing Requirements..."
-cd /RadioPlayerV3
+cd /Radio
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
 python3 main.py
